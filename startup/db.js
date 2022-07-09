ee0729f03e33af8d4045219c050cb3ed0f51c81b
KristianMeier <85857919+KristianMeier@users.noreply.github.com>
2022-07-09 08:58:49 +0200

    refactor

--- startup/db.js
+++ startup/db.js
@@ -1,9 +1,8 @@
-const winston = require('winston');
-const mongoose = require('mongoose');
-const config = require('config');
+const winston = require("winston");
+const mongoose = require("mongoose");
 
-module.exports = function() {
-  const db = config.get('db');
-  mongoose.connect(db)
-    .then(() => winston.info(`Connected to ${db}...`));
-}
+module.exports = function () {
+  const db =
+    "mongodb+srv://vidlyuser:1234@cluster0.3n5wy.mongodb.net/?retryWrites=true&w=majority";
+  mongoose.connect(db).then(() => winston.info(`Connected to ${db}...`));
+};
