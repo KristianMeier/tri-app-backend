646ef75d7dc59e316c96bf4382ab0f2c4a74282a
KristianMeier <85857919+KristianMeier@users.noreply.github.com>
2022-07-09 09:19:46 +0200

    opd

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 const winston = require("winston");
 const express = require("express");
-const config = require("config");
 const app = express();
 
 require("./startup/cors")(app);
 require("./startup/routes")(app);
 require("./startup/db")();
 
-const port = process.env.PORT || config.get("port");
+const port = process.env.PORT;
 const server = app.listen(port, () =>
   winston.info(`Listening on port ${port}...`)
 );
