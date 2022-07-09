fb37ba0d68f352afab5c86aabcc83490bb736dab
KristianMeier <85857919+KristianMeier@users.noreply.github.com>
2022-07-09 09:14:40 +0200

    lille ændring

--- startup/db.js
+++ startup/db.js
@@ -2,7 +2,6 @@ const winston = require("winston");
 const mongoose = require("mongoose");
 
 module.exports = function () {
-  const db =
-    "mongodb+srv://vidlyuser:1234@cluster0.3n5wy.mongodb.net/?retryWrites=true&w=majority";
+  const db = "mongodb+srv://vidlyuser:1234@cluster0.3n5wy.mongodb.net/";
   mongoose.connect(db).then(() => winston.info(`Connected to ${db}...`));
 };
